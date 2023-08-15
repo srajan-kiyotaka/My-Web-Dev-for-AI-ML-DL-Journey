86098d21fefa8ed1eed5f66938842d60b07509c0
srajan-kiyotaka <srajanstark.ash@gmail.com>
2023-08-15 09:27:09 +0530

    Completed Exercise 7.

---
+++ Java Script Course/Exercise 7/Cart/index.js
@@ -0,0 +1,23 @@
+let cartQty = 0;
+
+function updateCartQty(cartQty, addQty = 1) {
+    if(addQty > 0) {
+        if(cartQty < 10){
+            cartQty = (cartQty + addQty > 10)?(10):(cartQty + addQty);
+            console.log(`Cart Quantity: ${cartQty}`);
+        }
+    }
+    else{
+        if(cartQty > 0){
+            cartQty = (cartQty + addQty >= 0)?(cartQty + addQty):(0);
+            console.log(`Cart Quantity: ${cartQty}`);
+        }
+    }
+    if(cartQty === 0){
+        alert(`Cart is Empty!`);
+    }
+    else if(cartQty === 10){
+        alert(`Cart is Full!`);
+    }
+    return cartQty;
+}
---
+++ Java Script Course/Exercise 7/calculator/script.js
@@ -0,0 +1,8 @@
+let calculate = "";
+let result = 0;
+
+function update(calculate, str){
+    calculate += str;
+    console.log('Current Equation: ' + calculate);
+    return calculate;
+}
